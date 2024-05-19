47057cfc55be102f98a347e3a1ab67b51e6b7439
trechds <trechbusiness@gmail.com>
2024-05-19 01:44:56 -0300

    hamburger button

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,9 @@
 const navmenu = document.querySelector('.navmenu');
 const hamburger = document.querySelector('#hamburger');
+const nav = document.querySelector('nav');
 
 hamburger.addEventListener('click', () => {
-	navmenu.classList.toggle('open');
-	hamburger.classList.toggle('open');
+    navmenu.classList.toggle('open'); 
+    hamburger.classList.toggle('open'); 
+    nav.classList.toggle('hidden'); 
 });
